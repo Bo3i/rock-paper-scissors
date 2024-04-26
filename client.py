63c6341ac3c5ebc097aad3efdbece0ccaeab1831
Jakub Kindlik <s210871@sggw.edu.pl>
2024-04-26 23:09:05 +0200

    add multi round games

--- client.py
+++ client.py
@@ -15,24 +15,28 @@ def play():
         print('Cannot connect to host!')
         exit()
 
-
     def on_response(ch, method, properties, body):
-        resp = body.decode()
+        resp, p_id = body.decode().split(",")
         print(f"Playing against: {resp}!")
+        channel.queue_declare(queue=f"{player_name}{session_id}{p_id}")
+        channel.queue_declare(queue=f"{player_name}{p_id}won")
         def play_round():
             print('Please enter "r" for rock "p" for paper and "s" for scissors')
             player_input = input("Your choice: ")
             channel.basic_publish(exchange='',
-                                  routing_key=f"{player_name}{session_id}",
+                                  routing_key=f"{player_name}{session_id}{p_id}",
                                   body=player_input)
 
             def winner(ch, method, properties, body):
-                win, mov = body.decode().split(",")
+                win, mov, y_score, op_score = body.decode().split(",")
                 print(f"{resp} chose: {mov}")
-                if win == resp:
+                if win == "Tie":
+                    print("It's a tie!")
+                elif win == resp:
                     print(f"{win} wins!")
                 else:
                     print("You win!")
+                print(f"---Score--- \nYou  {y_score} : {op_score}  {resp}")
                 play_again = input('Do you want to play again? y/n: ')
                 if play_again == 'n':
                     connection.close()
@@ -41,7 +45,7 @@ def play():
                 else:
                     play_round()
 
-            channel.basic_consume(queue=f"{player_name}won", on_message_callback=winner, auto_ack=True)
+            channel.basic_consume(queue=f"{player_name}{p_id}won", on_message_callback=winner, auto_ack=True)
         play_round()
 
     player_name = input('Set your name: ')
@@ -50,17 +54,17 @@ def play():
     if session_id == '':
         print('You have to provide session id! Now exiting....')
         exit()
-    channel.queue_declare(queue=f"{player_name}{session_id}")
+
     channel.queue_declare(queue='start')
     channel.basic_publish(exchange='',
                           routing_key='start',
                           body=f"{session_id},{player_name}")
     channel.queue_declare(queue=player_name)
-    channel.queue_declare(queue=f"{player_name}won")
     channel.basic_consume(queue=player_name, on_message_callback=on_response, auto_ack=True)
     print("Message to server sent")
 
     print("Waiting for response...")
     channel.start_consuming()
+
 
 play()
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ channel = connection.channel()
 
 sessions = {}
 
+
 def callback(ch, method, properties, body):
     message = body.decode()
     print("Received:", message)
@@ -16,7 +17,7 @@ def callback(ch, method, properties, body):
             sessions[session_id].append(player_name)
         else:
             print("Session is full")
-            #channel.basic_publish(exchange='',
+            # channel.basic_publish(exchange='',
             #                      routing_key=player_name,
             #                      body='#ErR!')
     else:
@@ -27,63 +28,78 @@ def callback(ch, method, properties, body):
         for player in sessions[session_id]:
             if sessions[session_id][0] == player:
                 opponent = sessions[session_id][1]
+                p_id = 0
             else:
                 opponent = sessions[session_id][0]
+                p_id = 1
+            channel.queue_declare(queue=f"{player}{session_id}{p_id}")
+            channel.queue_declare(queue=f"{player}{p_id}won")
             channel.basic_publish(exchange='',
                                   routing_key=player,
-                                  body=opponent)
-        start_game(session_id)
+                                  body=f"{opponent},{p_id}")
+        start_game(session_id, [0, 0])
 
 
-def start_game(session_id):
+def start_game(session_id, score):
     player1_move, player2_move = '', ''
     recieved = [0, 0]
+    print(f"recieved: {recieved}")
 
     def recieve1(ch, method, properties, body):
         nonlocal player1_move, recieved
         player1_move = body.decode()
         print(f"Recieved {player1_move} form player {player1_name}")
         recieved[0] += 1
-        if recieved[0]+recieved[1] == 2:
+        if recieved[0] == recieved[1] and recieved[0]+recieved[1] != 0:
+            print(f"Recieved: {recieved}")
             print('Now checking the winner')
             play()
         else:
             print('Waiting for all to respond')
+            print(f"Recieved: {recieved}")
 
     def recieve2(ch, method, properties, body):
         nonlocal player2_move, recieved
         player2_move = body.decode()
         print(f"Recieved {player2_move} form player {player2_name}")
         recieved[1] += 1
-        if recieved[0]+recieved[1] == 2:
+        if recieved[0] == recieved[1] and recieved[0]+recieved[1] != 0:
+            print(f"Recieved: {recieved}")
             print('Checking the winner')
             play()
         else:
             print('Waiting for all to respond')
+            print(f"Recieved: {recieved}")
 
     player1_name = sessions[session_id][0]
     player2_name = sessions[session_id][1]
 
-    channel.basic_consume(queue=f"{player1_name}{session_id}", on_message_callback=recieve1, auto_ack=True)
-    channel.basic_consume(queue=f"{player2_name}{session_id}", on_message_callback=recieve2, auto_ack=True)
+    channel.basic_consume(queue=f"{player1_name}{session_id}0", on_message_callback=recieve1, auto_ack=True)
+    channel.basic_consume(queue=f"{player2_name}{session_id}1", on_message_callback=recieve2, auto_ack=True)
 
     def play():
         if player1_move != '' and player2_move != '':
             if player1_move == player2_move:
-                winner = "It's a tie!"
+                score[0] += 1
+                score[1] += 1
+                winner = "Tie"
             elif (player1_move == "r" and player2_move == "s") or \
                     (player1_move == "p" and player2_move == "r") or \
                     (player1_move == "s" and player2_move == "p"):
                 winner = player1_name
+                score[0] += 1
             else:
                 winner = player2_name
-            print(f"{winner} won!")
+                score[1] += 1
+            print(f"Player {winner} won!")
             channel.basic_publish(exchange='',
-                                  routing_key=f"{player1_name}won",
-                                  body=f"{winner}, {player2_move}")
+                                  routing_key=f"{player1_name}0won",
+                                  body=f"{winner}, {player2_move}, {score[0]}, {score[1]}")
             channel.basic_publish(exchange='',
-                                  routing_key=f"{player2_name}won",
-                                  body=f"{winner}, {player1_move}")
+                                  routing_key=f"{player2_name}1won",
+                                  body=f"{winner}, {player1_move}, {score[1]}, {score[0]}")
+            print("Starting new round")
+            start_game(session_id, score)
         else:
             print("------------")
             print("Wrong input!")
@@ -93,7 +109,7 @@ def start_game(session_id):
 
 result = channel.queue_declare(queue='start', exclusive=False)
 queue_name = result.method.queue
-#channel.queue_bind(exchange='amq.direct', queue=queue_name)
+# channel.queue_bind(exchange='amq.direct', queue=queue_name)
 
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
